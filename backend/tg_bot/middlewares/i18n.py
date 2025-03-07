2d1af1c2cd5cb2e875ac1fd3fb76a6c8ca56ef57
Grigorii <griger1011@gmail.com>
2025-03-07 23:43:31 +0300

    refactoring annotations

--- backend/tg_bot/middlewares/i18n.py
+++ backend/tg_bot/middlewares/i18n.py
@@ -1,10 +1,16 @@
-from typing import Any
+from typing import Any, Callable, Awaitable
 
 from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
 
 
 class TranslatorRunnerMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event, data) -> Any:
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any:
         user = data.get("event_from_user")
         if user is None:
             return await handler(event, data)
